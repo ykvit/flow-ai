16a0b017fb6d559ff0dc996ec0893fc834f91b29
Illia O. <yumkvitka+github@gmail.com>
2025-10-09 02:19:10 +0300

    fix(ci): frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 // This is the standard entry point for a React 18+ application.
